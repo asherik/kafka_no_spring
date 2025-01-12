43db0b809ffdcfceb2933453cc5a62062e946fc7
asher <alexetik@yandex.ru>
2025-01-12 14:27:01 +0000

    правки

---
+++ src/main/java/com/proj/kafka_no_spring/KafkaNoSpringApplication.java
@@ -0,0 +1,13 @@
+package com.proj.kafka_no_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaNoSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaNoSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/proj/kafka_no_spring/KafkaNoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proj.kafka_no_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaNoSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
